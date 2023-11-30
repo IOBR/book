93b0aa1a111f838eb302b9d47aae1f7c8449b31b
Dongqiang Zeng <dongqiangzeng0808@gmail.com>
2023-11-30 22:24:43 +0800

    update book

--- 0-buliding-IOBR-book.R
+++ 0-buliding-IOBR-book.R
@@ -5,3 +5,10 @@
 library(usethis)
 usethis::use_git()
 use_github(organisation = "DongqiangZeng0808")
+
+
+
+
+# 
+# use_build_ignore("5-update-gene-signatures.R")
+# use_git_ignore("5-update-gene-signatures.R")
